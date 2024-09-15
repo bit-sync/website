908d79fab3575c5a59294ae002a0a72d6e19b4e9
Extraskilled56 <zjones.2012@icloud.com>
2024-09-14 20:29:17 -0500

    fix

--- build/assets/js/36994c47.d18fbc94.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbit_sync_web=self.webpackChunkbit_sync_web||[]).push([[858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- build/assets/js/c15d9823.a0e139a3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbit_sync_web=self.webpackChunkbit_sync_web||[]).push([[146],{9328:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
