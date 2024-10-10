65d5933ef2c0e3fc00f140f036c73065f58acbdb
L1FE <arnaykumar@outlook.com>
2024-10-10 03:54:05 +0000

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -39,8 +39,8 @@ function Feature({Svg, title, description}) {
         <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">Bit Sync</Heading>
-        <p>Bit Sync is a software development company that also has cloud services.</p>
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
       </div>
     </div>
   );
